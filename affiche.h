8e2106108eb5133cdcacdce30b7cf58cf39bf496
Kristopher JULIO <JulioK@BTS-SN.LRQ>
2019-11-12 15:41:30 +0100

    j'ai ajouter des commentaire à mon .h

--- affiche.h
+++ affiche.h
@@ -20,7 +20,7 @@
 
 
 
-void initAfficheur(int adress);
+void initAfficheur(int adress); // fonction qui initialise un afficheur on place l'adresse en parametre
 void effacerAfficheur (int adress);
 void setEclairage(int adress,int valeur);
 
